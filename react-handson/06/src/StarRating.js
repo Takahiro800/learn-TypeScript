f7aff49ee0367ab493c9dc9d7ed1b5dcd05ba3b7
taemath <hirotaka081630@gmail.com>
2022-07-12 21:04:28 +0900

    feat(p.114): 6.1.2 再利用性を考慮したリファクタリング

--- react-handson/06/src/StarRating.js
+++ react-handson/06/src/StarRating.js
@@ -3,16 +3,16 @@ import Star from './Star';
 
 // const Star = ({ selected = false }) => <FaStar color={selected ? 'red' : 'grey'} />;
 
-export default function StarRating({ totalStars = 5 }) {
+export default function StarRating({ style = {}, totalStars = 5, ...props }) {
   const [selectedStars, setSelectedStars] = useState(0);
   return (
-    <>
+    <div style={{ padding: '5ps', ...style }} {...props}>
       {[...Array(totalStars)].map((n, i) => (
         <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i + 1)} />
       ))}
       <p>
         {selectedStars} of {totalStars}
       </p>
-    </>
+    </div>
   );
 }
