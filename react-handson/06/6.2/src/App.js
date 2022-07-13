d10b4768387dabc077f1aae06589b76b3d7cc46f
taemath <hirotaka081630@gmail.com>
2022-07-13 21:57:34 +0900

    feat(6.2.2): ユーザーの操作をコンポーネントツリーの下から上に伝える

--- react-handson/06/6.2/src/App.js
+++ react-handson/06/6.2/src/App.js
@@ -3,6 +3,18 @@ import colorData from './color-data.json';
 import ColorList from './ColorList';
 
 export default function App() {
-  const [colors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  const [colors, setColors] = useState(colorData);
+  return (
+    <ColorList
+      colors={colors}
+      onRateColor={(id, rating) => {
+        const newColors = colors.map((color) => (color.id === id ? { ...colors, rating } : color));
+        setColors(newColors);
+      }}
+      onRemoveColor={(id) => {
+        const newColors = colors.filter((color) => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  );
 }
--- react-handson/06/6.2/src/ColorList.js
+++ react-handson/06/6.2/src/ColorList.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Color from './Color';
 
-export default function ColorList({ colors = [] }) {
+export default function ColorList({ colors = [], onRemoveColor = (f) => f, onRateColor = (f) => f }) {
   if (!colors.length) return <div>No Colors Listed.</div>;
 
   return (
-    <div>
+    <div className="color-list">
       {colors.map((color) => (
-        <Color key={colors.id} {...color} />
+        <Color key={colors.id} {...color} onRemove={onRemoveColor} onRate={onRateColor} />
       ))}
     </div>
   );
