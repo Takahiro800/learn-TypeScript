8bae8bdb180cb311d7c326bfd9094b119762aeb9
taemath <hirotaka081630@gmail.com>
2022-07-16 05:18:53 +0900

    refactor(6.3.3): カスタムフック

--- react-handson/06/6.2/src/AddColorForm.js
+++ react-handson/06/6.2/src/AddColorForm.js
@@ -1,26 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useInput } from './hooks';
 
 export default function AddColorForm({ onNewColor = (f) => f }) {
-  const [title, setTitle] = useState('');
-  const [color, setColor] = useState('#000000');
+  const [titleProps, setTitle] = useInput('');
+  const [colorProps, setColor] = useInput('#000000');
 
   const submit = (e) => {
     e.preventDefault();
-    onNewColor(title, color);
+    onNewColor(titleProps.value, colorProps.value);
     setTitle('');
     setColor('');
   };
 
   return (
     <form onSubmit={submit}>
-      <input
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
-        type="text"
-        placeholder="color title ..."
-        required
-      />
-      <input value={color} onChange={(event) => setColor(event.target.value)} type="color" required />
+      <input {...titleProps} type="text" placeholder="color title ..." required />
+      <input {...colorProps} type="color" required />
       <button>ADD</button>
     </form>
   );
