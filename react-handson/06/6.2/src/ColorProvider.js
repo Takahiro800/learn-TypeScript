649dac17a5922e59952d15e11c5ce13dd13c7ed9
taemath <hirotaka081630@gmail.com>
2022-07-17 20:10:43 +0900

    refactor(6.4.3): コンテキストとステートの併用
    setColorsをコンテキストで扱うのはどんな操作も可能になり危険なので、必要な関数のみをコンテキストで公開するように修正

--- react-handson/06/6.2/src/ColorProvider.js
+++ react-handson/06/6.2/src/ColorProvider.js
@@ -5,5 +5,21 @@ export const ColorContext = createContext();
 
 export default function ColorProvider({ children }) {
   const [colors, setColors] = useState(colorData);
-  return <ColorContext.Provider value={{ colors, setColors }}>{children}</ColorContext.Provider>;
+  // return <ColorContext.Provider value={{ colors, setColors }}>{children}</ColorContext.Provider>;
+
+  const addColor = (title, color) =>
+    setColors([
+      ...colors,
+      {
+        id: v4(),
+        rating: 0,
+        title,
+        color,
+      },
+    ]);
+  const rateColor = (id, rating) => colors.map((color) => (color.id === id ? { ...color, rating } : color));
+
+  const removeColor = (id) => setColors(colors.filter((color) => color.id !== id));
+
+  return <ColorContext.Provider value={{ colors, addColor, removeColor, rateColor }}>{children}</ColorContext.Provider>;
 }
