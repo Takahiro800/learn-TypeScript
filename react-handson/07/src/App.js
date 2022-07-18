d719f3c7da129165cea0aea74909faabd4ed606a
taemath <hirotaka081630@gmail.com>
2022-07-18 17:28:43 +0900

    feat(p.155): useLayoutEffectの利用例

--- react-handson/07/src/App.js
+++ react-handson/07/src/App.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+import useWindowSize from './useWindowSize';
 
 export default function App() {
   const [val, set] = useState('');
   const [phrase, setPhrase] = useState('example phrase');
+  const [width, height] = useWindowSize();
 
   const createPhrase = () => {
     setPhrase(val);
     set('');
   };
+
+  // useLayoutEffectは描画関数呼び出し後、描画完了前に実行される
+  useLayoutEffect(() => console.log('useLayoutEffect'));
+
+  // 空にすることで初回描画時のみに実行する
+  useEffect(() => {
+    console.log('only once after initial render by useEffect');
+  }, []);
 
   useEffect(() => {
     console.log(`typing "${val}"`);
@@ -22,16 +32,13 @@ export default function App() {
     console.log('either val or phrase has changed');
   }, [val, phrase]);
 
-  // 空にすることで初回描画時のみに実行する
-  useEffect(() => {
-    console.log('only once after initial render');
-  }, []);
-
   return (
     <>
       <label>Favorite phrase:</label>
       <input value={val} placeholder={phrase} onChange={(e) => set(e.target.value)} />
       <button onClick={createPhrase}>send</button>
+      <p>{width}</p>
+      <p>{height}</p>
     </>
   );
 }
