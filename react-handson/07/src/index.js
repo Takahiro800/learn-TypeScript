b1a90d3068a80d6c9af6137be940bac4df34562e
taemath <hirotaka081630@gmail.com>
2022-07-18 17:50:45 +0900

    feat(7.3 usereducer): coheckboxコンポーネントを作成

--- react-handson/07/src/index.js
+++ react-handson/07/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import Checkbox from './checkBox';
 
-render(<App />, document.getElementById('root'));
+// render(<App />, document.getElementById('root'));
+render(<Checkbox />, document.getElementById('root'));
