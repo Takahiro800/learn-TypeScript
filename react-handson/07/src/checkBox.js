c85203de9077e1237319e3b1a7b6728c9cdc2ba6
taemath <hirotaka081630@gmail.com>
2022-07-20 06:59:58 +0900

    feat(p.161): useReducerを使った複雑なステート管理
    buttonをクリックすると、admin: trueのみでuserデータ全体を更新してしまう例

--- react-handson/07/src/checkBox.js
+++ react-handson/07/src/checkBox.js
@@ -5,11 +5,13 @@ export default function Checkbox() {
 
   // 新しく値をセットするのではなく、現在のステート値を元に更新する際にuseReducerを活用
   const [checked, toggle] = useReducer((checked) => !checked, false);
+  const [number, setNumber] = useReducer((number, addNumber) => number + addNumber, 0);
 
   return (
     <>
       <input type="checkbox" value={checked} onChange={toggle} />
       {checked ? 'checked' : 'not checked'}
+      <h1 onClick={() => setNumber(1)}>{number}</h1>
     </>
   );
 }
--- react-handson/07/src/index.js
+++ react-handson/07/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
 import Checkbox from './checkBox';
+import User from './user';
 
 // render(<App />, document.getElementById('root'));
-render(<Checkbox />, document.getElementById('root'));
+// render(<Checkbox />, document.getElementById('root'));
+render(<User />, document.getElementById('root'));
