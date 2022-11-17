ba7c6d7436b585d64bf0bb3a0f40afa7de40ea15
taemath <hirotaka081630@gmail.com>
2022-11-17 16:19:35 +0900

    feat(type-react-next/3.5.2): p.94~95

--- type-react-next/03/react-sample/src/index.tsx
+++ type-react-next/03/react-sample/src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from './components/Hello';
+import { Parent } from './components/Parent';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <Hello></Hello> */}
+    <Parent />
   </React.StrictMode>
 );
 
