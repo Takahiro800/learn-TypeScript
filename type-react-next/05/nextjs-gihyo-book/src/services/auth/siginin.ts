992e675b6811b47b1cd9c275dc0a7c8b36dc98d8
taemath <hirotaka081630@gmail.com>
2022-11-23 23:15:43 +0900

    feat(type-react-next/p.210~212): 6.2.2 APIクライアントの関数

---
+++ type-react-next/05/nextjs-gihyo-book/src/services/auth/siginin.ts
@@ -0,0 +1,30 @@
+import { ApiContext, User } from 'types'
+
+import { fetcher } from 'utils'
+
+export type SigninParams = {
+  /**
+   * ユーザー名
+   * サンプルユーザーの名前は "user"
+   */
+  username: string
+  password: string
+}
+
+const signin = async (
+  context: ApiContext,
+  params: SigninParams,
+): Promise<User> => {
+  return await fetcher(
+    `${context.apiRootUrl.replace(/\/$/g, '')}/auth/signin`,
+    {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(params),
+    },
+  )
+}
+export default signin
